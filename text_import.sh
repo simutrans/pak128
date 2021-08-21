992df71f8a65cf11ccb1d28037c945836347a06b
Roboron3042 <robertoms258@gmail.com>
2021-08-21 22:28:05 +0200

    Script: Add text generation before download

--- text_import.sh
+++ text_import.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PAK=pak128
+wget -q --post-data "version=313&choice=all&submit=Export!"  --delete-after https://translator.simutrans.com/script/main.php?page=wrap
 wget -O texts.zip https://makie.de/translator/data/tab/language_pack-Scenario+Tutorial+$PAK.zip
 mkdir tmp && cd tmp
 unzip ../texts.zip
