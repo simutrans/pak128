283a3e98327c83f0abcd59af5c1066dcd6b902a5
prissi <prissi@github.com>
2024-04-17 15:50:46 +0900

    Put all text import in one place

--- text_import.sh
+++ text_import.sh
@@ -1,30 +1,55 @@
 #!/bin/bash
 
-wget -q --post-data "version=19&choice=all&submit=Export!"  --delete-after http://translator.simutrans.com/script/main.php?page=wrap
-wget -O texts.zip http://makie.de/translator/data/tab/language_pack-pak128.zip
+PAK=pak128
 
+echo "Downloading pak texts"
 rm -rf tmp
 mkdir tmp
 cd tmp
-unzip ../texts.zip
+wget -q --post-data "version=19&choice=all&submit=Export!"  --delete-after http://translator.simutrans.com/script/main.php?page=wrap
+wget -O texts.zip http://makie.de/translator/data/tab/language_pack-$PAK.zip
+unzip texts.zip
 
 # only copy changed texts
 for f in ./*.tab; do
   cat $f | grep "^[^#]" > $f.tmp
-  cat ../pak128.prototype/text/$f | grep "^[^#]" > ../pak128.prototype/text/$f.tmp
-  a=$(diff -wq $f.tmp ../pak128.prototype/text/$f.tmp)
+  a=`cat ../pak128.prototype/text/$f | grep "^[^#]" | diff -wq $f.tmp -`
   if [[ ! -z "$a" ]] ; then
     echo "Moving $f to pak texts"
     mv $f ../pak128.prototype/text/
   fi
-  rm ../pak128.prototype/text/$f.tmp
 done
 
 # cleanup
 cd ..
 rm -fr tmp
-rm texts.zip
 
 # download scenario texts
+echo "Downloading scenario texts"
 cd pak128.prototype/scenario/tutorial
-./text_import.sh
+mkdir tmp
+cd tmp
+
+wget -q --post-data "version=313&choice=all&submit=Export!"  --delete-after http://translator.simutrans.com/script/main.php?page=wrap
+wget -O texts.zip http://makie.de/translator/data/tab/language_pack-Scenario+Tutorial+$PAK.zip
+unzip texts.zip
+rm texts.zip
+
+# protect en.tab (why???)
+rm en.tab
+
+# only copy changed texts
+for f in ./*.tab; do
+  cat $f | grep "^[^#]" > $f.tmp
+  a=`cat ../$f | grep "^[^#]" | diff -wq $f.tmp -`
+  if [[ ! -z "$a" ]] ; then
+    echo "Moving $f to tutorial texts"
+    mv $f ..
+  fi
+done
+rm *.tab *.tab.tmp
+rm _*.txt
+# copy the rest
+cp -r * ..
+cd ..
+rm -rf tmp
