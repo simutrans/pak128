31645eadb66dfce52eb08eff29c1996209c2648e
prissi <prissi@github.com>
2024-04-17 15:06:52 +0900

    Fix text import

--- text_import.sh
+++ text_import.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-PAK=pak128
-wget -q --post-data "version=313&choice=all&submit=Export!"  --delete-after https://translator.simutrans.com/script/main.php?page=wrap
-wget -O texts.zip https://makie.de/translator/data/tab/language_pack-Scenario+Tutorial+$PAK.zip
-mkdir tmp && cd tmp
+
+wget -q --post-data "version=19&choice=all&submit=Export!"  --delete-after https://translator.simutrans.com/script/main.php?page=wrap
+wget -O texts.zip https://makie.de/translator/data/tab/language_pack-pak128.zip
+
+rm -rf tmp
+mkdir tmp
+cd tmp
 unzip ../texts.zip
-rsync -avr --exclude='en.tab' --exclude='_objectlist.txt' --exclude='_translate_users.txt' * ..
+
+# only copy changed texts
+for f in ./*.tab; do
+  cat $f | grep "^[^#]" > $f.tmp
+  cat ../pak128.prototype/text/$f | grep "^[^#]" > ../pak128.prototype/text/$f.tmp
+  a=$(diff -wq $f.tmp ../pak128.prototype/text/$f.tmp)
+  if [[ ! -z "$a" ]] ; then
+    echo "Moving $f to pak texts"
+    mv $f ../pak128.prototype/text/
+  fi
+  rm ../pak128.prototype/text/$f.tmp
+done
+
+# cleanup
 cd ..
-rm -r tmp
+rm -fr tmp
 rm texts.zip
 
+# download scenario texts
+cd pak128.prototype/scenario/tutorial
+./text_import.sh
